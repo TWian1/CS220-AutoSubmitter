a0092ba8d8e63c72830f705051d7a3e8ac106112
Ian <80492876+TWian1@users.noreply.github.com>
2026-09-21 20:39:14 -0400

    Add Nothing to commit message

--- submit.bat
+++ submit.bat
@@ -19,7 +19,7 @@ if /i "%~2"=="-nz" set NOZIP=1
 if /i "%~3"=="-nz" set NOZIP=1
 if /i "%~2"=="-ng" set GITCOMMIT=0
 if /i "%~3"=="-ng" set GITCOMMIT=0
-ssh %SSH_USR%@remote.cs.binghamton.edu "cd %IN_DIR% && MADE=0 && if [ ! -f README.md ]; then cp ../README.md.tmpl README.md && sed -i '1s|.*|#    %~1|' README.md && MADE=1; fi && if [ %GITCOMMIT% -eq 1 ]; then git add . && { git diff --cached --quiet || git commit -m 'completed %~1'; } && git push; fi && cd ../ && if [ $MADE -eq 1 ] || [ %NOZIP% -eq 0 ] || [ ! -f %IN_DIR%.zip ]; then %ZIP_DIR% %~1-sol; fi"
+ssh %SSH_USR%@remote.cs.binghamton.edu "cd %IN_DIR% && MADE=0 && if [ ! -f README.md ]; then cp ../README.md.tmpl README.md && sed -i '1s|.*|#    %~1|' README.md && MADE=1; fi && if [ %GITCOMMIT% -eq 1 ]; then git add . && if git diff --cached --quiet; then echo 'Nothing new to commit.'; else git commit -m 'completed %~1'; fi && git push; fi && cd ../ && if [ $MADE -eq 1 ] || [ %NOZIP% -eq 0 ] || [ ! -f %IN_DIR%.zip ]; then %ZIP_DIR% %~1-sol; fi"
 if errorlevel 1 (
     echo Remote zip step failed.
     pause
